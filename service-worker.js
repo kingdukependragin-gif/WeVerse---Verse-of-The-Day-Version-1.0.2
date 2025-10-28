27ab9f974e18a1653603b1df6bd9c0fcd076ffa2
kingdukependragin-gif <kingdukependragin@gmail.com>
2025-10-28 10:26:00 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "weverse-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./faq.html",
+  "./jesus-paid-it-all-piano-improvisation-314836.mp3",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
