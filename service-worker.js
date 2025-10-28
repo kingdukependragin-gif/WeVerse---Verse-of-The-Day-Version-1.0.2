3c62260918c80bca9a5e550de5537a0b68bd9842
kingdukependragin-gif <kingdukependragin@gmail.com>
2025-10-28 10:35:02 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,43 @@
 const CACHE_NAME = "weverse-cache-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./about.html",
-  "./faq.html",
-  "./jesus-paid-it-all-piano-improvisation-314836.mp3",
-  "./manifest.json"
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/index.html",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/about.html",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/faq.html",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/jesus-paid-it-all-piano-improvisation-314836.mp3",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/manifest.json",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/icon-192.png",
+  "/WeVerse---Verse-of-The-Day-Version-1.0.2/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching files...");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
-    )
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Deleting old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
